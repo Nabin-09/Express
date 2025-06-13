5de3fff69c866c8abea305293e3b4c10e752e169
Nabin <sharmanabin.0910@gmail.com>
2025-06-13 11:44:49 +0530

    Route Parameters

--- 3_HandlingURLs/notes.md
+++ 3_HandlingURLs/notes.md
@@ -7,3 +7,21 @@ www.nabinsharma.dev is a user friendly name of IP address of the server
 / - this is the path and / is root path , /about , /contact can be some more paths
 and /contacts/linkedIn is a nested path
 
+## Route parameters
+
+eg - nabinsharma.dev/about?userID=1&a=2
+these are extra info that can be passed with our URL , everything after the '?' is a query
+parameter , we can clearly check the route and what info is coming from user.
+
+eg - https://www.google.com/search?q=javascript+interview+questions&rlz=1C1CHBF_enIN1143IN1143&oq=javascript+intervie&gs_lcrp=EgZjaHJvbWUqEAgAEAAYkQIYsQMYgAQYigUyEAgAEAAYkQIYsQMYgAQYigUyDQgBEAAYkQIYgAQYigUyBggCEEUYOTINCAMQABiRAhiABBiKBTIHCAQQABiABDINCAUQABiRAhiABBiKBTINCAYQABiRAhiABBiKBTIHCAcQABiABDIHCAgQABiABDIHCAkQABiABNIBCDU4MjJqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8
+
+This is a production grade google URL you can see there are so many route paramters passed
+
+In our current server when I sent request to /about using route params , this is what I got in log file:
+```1749795079856: /about?myName=Nabin New request received ```
+and server said "404 not found"
+
+## url (A node package)
+
+
+
---
+++ 3_HandlingURLs/server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs');
+
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()}: ${req.url} New request received\n`;
+
+    console.log('New request received');
+    // console.log(req.headers); // You can uncomment this to see request headers
+
+    // Append each request log to log.txt file
+    fs.appendFile('log.txt', log, (err, data) => {
+        // if(err) console.log("ERROR", err);
+        // else console.log("Hello from server again")
+
+        if (err) {
+            console.error("ERROR", err);
+            res.statusCode = 500;
+            res.end("Internal Server Error");
+            return;
+        }
+
+        // Handle routes
+        switch (req.url) {
+            case '/':
+                res.end('Homepage');
+                break;
+            case '/about':
+                res.end('I am Nabin Sharma');
+                break;
+            default:
+                res.statusCode = 404;
+                res.end('404 not found');
+        }
+    });
+
+    /*
+    Example headers:
+    {
+      host: 'localhost:8000',
+      connection: 'keep-alive',
+      'cache-control': 'max-age=0',
+      'sec-ch-ua': '"Google Chrome";v="137", "Chromium";v="137", "Not/A)Brand";v="24"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'upgrade-insecure-requests': '1',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
+      accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,
+    }
+    */
+
+    // Removed: res.end("Hello from server"); — it would cause duplicate response error if uncommented
+});
+
+myServer.listen(8000, () => {
+    console.log('Server running');
+});
