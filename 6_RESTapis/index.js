2b746f5434723692ca78372b4bc99386da4f1ab4
Nabin <sharmanabin.0910@gmail.com>
2025-06-14 14:09:39 +0530

    GET /users

--- 6_RESTapis/index.js
+++ 6_RESTapis/index.js
@@ -4,6 +4,13 @@ const users = require("./MOCK_DATA.json")
 const app = express();
 
 const PORT = 3000
+
+//routes 
+app.get('/users' , (req , res)=>{
+    return res.json(users);
+})
+
+
 app.listen(PORT , ()=>{
     console.log(`Server running in ${PORT}`);
 })
