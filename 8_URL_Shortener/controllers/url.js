7c98da904ce587568a3c535309df42ce7f9d1e5e
Nabin <sharmanabin.0910@gmail.com>
2025-06-18 10:24:25 +0530

    Setting up routes and controllers

---
+++ 8_URL_Shortener/controllers/url.js
@@ -0,0 +1,17 @@
+const {nanoid} = require('nanoid')
+const URL = require('../models/url')
+
+async function handleGenerateNewShortURL(req , res){
+    const shortID = nanoid(8);
+    if(!body.url) return res.status(400).json({error : 'url is required'})
+    await URL.create({
+        shortID : shortID,
+        redirectURL : body.url,
+        visitHistory : [],
+    });
+    return res.json({id : shortID});
+}
+
+module.exports = {
+    handleGenerateNewShortURL,
+}
--- 8_URL_Shortener/index.js
+++ 8_URL_Shortener/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const { connectToMongoDB } = require("./connect")
+const urlRoute = require('./routes/url')
 
 const app = express();
 
 const PORT = 8001
+app.use("/url" , urlRoute);
+connectToMongoDB()
 
 app.listen(PORT , ()=>{
     console.log(`Server running at ${PORT}`)
